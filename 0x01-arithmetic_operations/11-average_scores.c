1df2cc47de19d74a1e8e600a28fb0907ca95daa8
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 01:37:38 +0300

    Average

---
+++ 0x01-arithmetic_operations/11-average_scores.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+{
+    int scores[3];
+    for (int i=0; i<3;i++)
+    {
+        scores[i]= get_int("Score: ");
+    }
+    {
+        printf("Average: %f\n", (scores[0]+scores[1]+scores[2])/3.0);
+    }
+}
