285da7979431c149c7806adf353213b64b00ff75
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 00:50:25 +0300

    Mario alternative

---
+++ 0x02-loops/3-mario.c
@@ -0,0 +1,21 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+
+    do
+    {
+        n = get_int("Width: ");
+    }
+    while (n < 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("?");
+    }
+    {
+        printf("\n");
+    }
+}
