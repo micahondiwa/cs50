cd88386583efd92da866900d15ebe61fbb19b77a
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 01:01:20 +0300

    Print bricks

---
+++ 0x02-loops/4-brick.c
@@ -0,0 +1,22 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+    int n;
+
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+    printf("#");
+    }
+    printf("\n");
+    }
+}
