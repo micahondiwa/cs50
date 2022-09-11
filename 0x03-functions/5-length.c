2b34b7287c8d185c53eb4f533e7e1a1294dc6c12
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 02:19:01 +0300

    length

---
+++ 0x03-functions/5-length.c
@@ -0,0 +1,22 @@
+#include<cs50.h>
+#include <stdio.h>
+
+int length = string_length(name);
+
+int main (void)
+{
+    string name = get_string("Name: ");
+    int length = string_length(name);
+    printf("%i\n", length);
+}
+
+
+int string_length(string s)
+{
+    int i = 0;
+    while (s[i] !='\0')
+    {
+        i++;
+    }
+    return i;
+}
