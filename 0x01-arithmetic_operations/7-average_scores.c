243b5ceb5c781c47e919aa4e4b63b40dae5c0d88
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 01:29:02 +0300

    Averages

---
+++ 0x01-arithmetic_operations/7-average_scores.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int score1 = 72;
+    int score2 = 73;
+    int score3 = 33;
+
+    {
+        printf("Average: %i\n", (score1 + score2 + score3) / 3);
+    }
+}
