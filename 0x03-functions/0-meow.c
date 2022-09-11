eeccecb90a71d9623b45f01ebb1f8184e7166103
Mikondiwa <mikondiwa@gmail.com>
2022-09-11 23:44:24 +0300

    meow function

---
+++ 0x03-functions/0-meow.c
@@ -0,0 +1,14 @@
+#include<cs50.h>
+#include<stdio.h>
+
+void meow (void)
+{
+    printf("meow\n");
+}
+int main(void)
+{
+    for (int i = 0; i <= 3; i++)
+    {
+        printf("meow\n");
+    }
+}
