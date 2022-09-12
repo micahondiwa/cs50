ab3d9cdd12bae4c7807d5d4a2767735f8bc65f47
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 22:05:24 +0000

    CL arg

---
+++ 0x02-loops/11-cl_arg.c
@@ -0,0 +1,14 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        printf("Hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("Hello, world\n");
+    }
+}
