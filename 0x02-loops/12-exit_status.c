067c095ea71d7ff10a236f4a0d40a6398cfaa40b
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 22:09:48 +0000

    Exit status

---
+++ 0x02-loops/12-exit_status.c
@@ -0,0 +1,13 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Error: Missing command-line argument.\n");
+        return 1;
+    }
+    printf("Hello, world.\n");
+    return 0;
+}
