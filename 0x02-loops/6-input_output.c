a1441be4b9fae0a3f296e908ce0af7e156be29fe
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 21:28:40 +0000

    Same input, output

---
+++ 0x02-loops/6-input_output.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+
+int main(void)
+{
+    string s = get_string("Input: ");
+
+    printf("Output: ");
+    for (int i = 0; i < strlen(s); i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+}
