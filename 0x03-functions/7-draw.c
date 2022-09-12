d695f635dc2ef771deceb0c59f43179f640fec58
Mikondiwa <mikondiwa@gmail.com>
2022-09-12 23:16:15 +0000

    Draw tringle with #

---
+++ 0x03-functions/7-draw.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<cs50.h>
+
+void draw(int n);
+
+int main(void)
+{
+    int height = get_int("Height: ");
+
+    draw(height);
+}
+
+void draw(int n)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+    draw(n-1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
