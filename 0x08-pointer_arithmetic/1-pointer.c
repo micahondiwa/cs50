c237ff8683433f68a17da7c29943a9daf9fc6509
Mikondiwa <mikondiwa@gmail.com>
2022-09-13 00:04:06 +0000

    Pointer integers

---
+++ 0x08-pointer_arithmetic/1-pointer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int numbers[] = {3,4,5,67,7,0};
+
+    printf("%i\n", *numbers);
+    printf("%i\n", *(numbers + 1));
+    printf("%i\n", *(numbers + 2));
+    printf("%i\n", *(numbers + 3));
+    printf("%i\n", *(numbers + 4));
+    printf("%i\n", *(numbers + 5));
+    printf("%i\n", *(numbers + 6));
+}
