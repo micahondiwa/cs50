5389d19a248889c02b001116271d7fdd2a4af94e
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 15:56:26 +0300

    Update 6-sale_price.c

--- 0x01-arithmetic_operations/6-sale_price.c
+++ 0x01-arithmetic_operations/6-sale_price.c
@@ -1,6 +1,12 @@
 #include<cs50.h>
 #include<stdio.h>
 
+/**
+* main: Promts the user to enter the regular sales price, and calculates the actual sales price after the discount.
+* 
+* Return: no return.
+*/
+
 int main(void)
 {
     float regular = get_float("Regular Price: ");
