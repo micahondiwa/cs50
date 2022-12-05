da17ff421ca72ff88e78c95a2c533c55d12a1187
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 15:42:40 +0300

    Update 2-points_calc.c

--- 0x01-arithmetic_operations/2-points_calc.c
+++ 0x01-arithmetic_operations/2-points_calc.c
@@ -1,9 +1,15 @@
 #include<cs50.h>
 #include<stdio.h>
 
+/**
+* The program promts the user to enter the number of points and compares them with 2. 
+*
+* Return: no return.
+*/
+
 int main(void)
 {
-    int points =get_int("How many points did you lose? ");
+    int points = get_int("How many points did you lose? ");
     const int MINE = 2;
 
     if (points < MINE)
