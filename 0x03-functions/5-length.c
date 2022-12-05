c324b44aaf82626b087c91846351ca5de311fa69
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 18:21:53 +0300

    Update 5-length.c

--- 0x03-functions/5-length.c
+++ 0x03-functions/5-length.c
@@ -1,6 +1,13 @@
 #include<cs50.h>
 #include <stdio.h>
 
+/**
+* string_length: Calculates the length of a string.
+*
+*@name: the input string from the user.
+*Return: length of the string. 
+*/
+
 int length = string_length(name);
 
 int main (void)
@@ -14,7 +21,7 @@ int main (void)
 int string_length(string s)
 {
     int i = 0;
-    while (s[i] !='\0')
+    while (s[i] != '\0')
     {
         i++;
     }
