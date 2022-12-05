795e3b9b7d76f5227b3cda06b6cb65b69f1f7077
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 17:35:32 +0300

    Update 3-sale_price.c

--- 0x03-functions/3-sale_price.c
+++ 0x03-functions/3-sale_price.c
@@ -1,6 +1,13 @@
 #include<cs50.h>
 #include<stdio.h>
 
+/**
+* discount: calculates the actual sales price after the discount.
+*
+* @price: the selling price.
+* @percentage: the percentage discount.
+*Return: sales price after the discoun.
+*/
 float discount(float price, int percentage);
 
 int main (void)
@@ -9,9 +16,11 @@ int main (void)
     int percent_off = get_int("Percent off: ");
     float sale = discount(regular, percent_off);
 
-    printf("Sale price: %.2f\n",sale);
+    printf("Sale price: %.2f\n", sale);
 }
+
 float discount(float price, int percentage)
 {
+    
     return price * (100 - percentage) / 100;
 }
