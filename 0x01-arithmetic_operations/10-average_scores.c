bc3deb31719196e821ee71661a8ea25c9e434ac7
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 16:10:56 +0300

    Update 10-average_scores.c

--- 0x01-arithmetic_operations/10-average_scores.c
+++ 0x01-arithmetic_operations/10-average_scores.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
 
+/**
+* main: prints the average of 3 scores.
+*
+* Return: no return.
+*/
+
 int main(void)
 
 {
     int scores[3];
-    scores[0]= get_int("Score: ");
-    scores[1]=get_int("Score: ");
-    scores[2]=get_int("Score: ");
+    scores[0] = get_int("Score: ");
+    scores[1] = get_int("Score: ");
+    scores[2] = get_int("Score: ");
 
     {
-        printf("Average: %f\n", (scores[0]+scores[1]+scores[2])/3.0);
+        printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
     }
 }
