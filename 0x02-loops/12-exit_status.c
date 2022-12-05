5e633873d9080dba71b30cd26ffd5aeeea8d5a0f
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 17:19:32 +0300

    Update 12-exit_status.c

--- 0x02-loops/12-exit_status.c
+++ 0x02-loops/12-exit_status.c
@@ -1,6 +1,13 @@
 #include<cs50.h>
 #include<stdio.h>
 
+/**
+* main: Prints 'hello, world' if user types on the command-line, and 
+* 'Missing command-line argument' if no argument is entered.
+*
+* Return: 1 if success, 0 otherwise.
+*/
+
 int main(int argc, string argv[])
 {
     if (argc != 2)
@@ -8,6 +15,7 @@ int main(int argc, string argv[])
         printf("Error: Missing command-line argument.\n");
         return 1;
     }
+    
     printf("Hello, world.\n");
     return 0;
 }
