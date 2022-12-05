ea67b280b1b78788f0e50e44d5c8d288155ec8a3
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 16:09:06 +0300

    Update 9-average_scores.c

--- 0x01-arithmetic_operations/9-average_scores.c
+++ 0x01-arithmetic_operations/9-average_scores.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+/**
+* main: prints the average of 3 scores.
+* 
+* Return: no return.
+*/
+
 int main(void)
 
 {
     int scores[3];
-    scores[0]= 72;
-    scores[1]=73;
-    scores[2]=33;
-
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+    
     {
-        printf("Average: %f\n", (scores[0]+scores[1]+scores[2])/3.0);
+        printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
     }
 }
