ae4e279d29831a4b1d1f107375070049e0d150d1
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 16:56:22 +0300

    Update 5-length.c

--- 0x02-loops/5-length.c
+++ 0x02-loops/5-length.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<cs50.h>
+
+/**
+* main: rints out the length of a name/variable entered by a user
+* 
+* Return: no return.
+*/
 
 int main (void)
 {
     string name = get_string("Name: ");
 
-    int i =0;
-    while (name[i]!='\0')
+    int i = 0;
+    while (name[i] != '\0')
     {
         i++;
-    }
-    printf("%i\n",i);
+    } 
+    
+    printf("%i\n", i);
 }
