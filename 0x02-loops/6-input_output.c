1fba4ebcbedd7d30d309b430c8febba37d7265a6
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 17:00:59 +0300

    Update 6-input_output.c

--- 0x02-loops/6-input_output.c
+++ 0x02-loops/6-input_output.c
@@ -2,14 +2,22 @@
 #include<cs50.h>
 #include<string.h>
 
+/**
+* main: Printing an output same as input.
+*
+* Return: no return.
+*/
+
 int main(void)
 {
     string s = get_string("Input: ");
 
     printf("Output: ");
+    
     for (int i = 0; i < strlen(s); i++)
     {
         printf("%c", s[i]);
     }
+    
     printf("\n");
 }
