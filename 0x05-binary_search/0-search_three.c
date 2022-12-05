0b0efbdfbaf2cd5e0b3cc246cd611ec27f71b78a
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 18:33:23 +0300

    Update 0-search_three.c

--- 0x05-binary_search/0-search_three.c
+++ 0x05-binary_search/0-search_three.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<cs50.h>
 
+/**
+* main: Searching for the number 3 in the list.
+* 
+* Return: no return.
+*/
+
 int main(void)
 {
     int numbers [] = {9,3,4,5,7,8,9,2};
