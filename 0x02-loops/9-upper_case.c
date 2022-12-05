31c69819810678f0ccd93379f3615f9cd966dad5
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 17:09:46 +0300

    Update 9-upper_case.c

--- 0x02-loops/9-upper_case.c
+++ 0x02-loops/9-upper_case.c
@@ -3,21 +3,27 @@
 #include<stdio.h>
 #include<string.h>
 
+/**
+* main: converts a letter to uppercase
+*
+* Return: no return.
+*/
+
 int main(void)
 {
     string s = get_string("Before: ");
 
     printf("After: ");
-    for (int i = 0,n = strlen(s); i < n; i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
         if (islower(s[i]))
         {
-            printf("%c", s[i]-32);
+            printf("%c", s[i] - 32);
         }
         else
         {
             printf("%c", s[i]);
         }
-        printf("");
+        printf("\n");
     }
 }
