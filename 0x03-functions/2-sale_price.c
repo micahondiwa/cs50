21eee788d8b334ca58b3386205fe4cb720c36407
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 17:32:10 +0300

    Update 2-sale_price.c

--- 0x03-functions/2-sale_price.c
+++ 0x03-functions/2-sale_price.c
@@ -1,12 +1,20 @@
 #include<cs50.h>
 #include<stdio.h>
 
+/**
+* discount: calculates the actual sales price after the discount.
+*
+* @price: the selling price.
+*Return: sales price after the discoun.
+*/
+
 float discount(float price);
 
 int main(void)
 {
     float regular = get_float("Regular Price: ");
     float sale = discount(regular);
+    
     printf("Sale price: %.2f\n", sale);
 }
 float discount(float price)
