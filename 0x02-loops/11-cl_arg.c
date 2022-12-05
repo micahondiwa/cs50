1d77f8d8bc6e1066fe09fce82e9c0884a95f9eb2
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 17:15:26 +0300

    Update 11-cl_arg.c

--- 0x02-loops/11-cl_arg.c
+++ 0x02-loops/11-cl_arg.c
@@ -1,6 +1,12 @@
 #include<cs50.h>
 #include<stdio.h>
 
+/**
+* Main: Prints "Hello, World" using if, else condition.
+*
+* Return: no return.
+*/
+
 int main(int argc, string argv[])
 {
     if (argc == 2)
