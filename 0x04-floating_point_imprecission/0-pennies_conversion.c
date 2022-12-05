44162028823ff5b7383eb57709640e614f6ffe8a
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-05 18:26:53 +0300

    Update 0-pennies_conversion.c

--- 0x04-floating_point_imprecission/0-pennies_conversion.c
+++ 0x04-floating_point_imprecission/0-pennies_conversion.c
@@ -2,11 +2,17 @@
 #include<cs50.h>
 #include<math.h>
 
+/**
+* main: converts dollars to pennies
+*
+* Return: no return.
+*/
+
 int main(void)
 
 {
     float amount = get_float("Dollar amount: ");
     int pennies = round(amount * 100);
 
-    printf("pennies: %i\n",pennies);
+    printf("pennies: %i\n", pennies);
 }
