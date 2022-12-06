8853ba4943fe8e05713ed1124e01c547c3f98ccb
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-06 16:02:06 +0300

    Update 4-swap.c

--- 0x09-dynamic_memory_allocation/4-swap.c
+++ 0x09-dynamic_memory_allocation/4-swap.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+* swap: swaps the values of 2 integers.
+* @a: the first integer.
+* @b: the second integer.
+*
+* Return: no return.
+*/
+
 void swap(int *a, int *b);
 
 int main(void)
@@ -8,9 +16,9 @@ int main(void)
     int x = 1;
     int y = 2;
 
-    printf("x is %i, y is %i\n", x,y);
+    printf("x is %i, y is %i\n", x, y);
     swap(&x, &y);
-    printf("x is %i, y is %i\n", x,y);
+    printf("x is %i, y is %i\n", x, y);
 }
 
 void swap(int *a, int *b)
