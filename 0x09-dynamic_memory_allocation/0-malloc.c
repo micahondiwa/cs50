23579195fa578ff492aa2c05e8932ae3b33ca05b
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-06 15:52:49 +0300

    Update 0-malloc.c

--- 0x09-dynamic_memory_allocation/0-malloc.c
+++ 0x09-dynamic_memory_allocation/0-malloc.c
@@ -4,12 +4,18 @@
 #include<stdlib.h>
 #include<string.h>
 
+/**
+* main: getes a string, allocates a memory for it and change it to uppercase.
+*
+* Return: no return.
+*/
+
 int main(void)
 {
     char *s = get_string("s: ");
     char *t = get_string("t: ");
 
-    strcpy(t,s);
+    strcpy(t, s);
 
     t[0] = toupper(t[0]);
 
