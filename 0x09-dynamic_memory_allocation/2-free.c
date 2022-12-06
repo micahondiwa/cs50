7cc81cf5550487fb6949d0b34dbbb02ada80fc2c
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-06 15:57:32 +0300

    Update 2-free.c

--- 0x09-dynamic_memory_allocation/2-free.c
+++ 0x09-dynamic_memory_allocation/2-free.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+* main: allocates memory equal to the size of the integer and frees the memory. 
+*
+* Return: no return.
+*/
+
 int main(void)
 {
     int *x = malloc(3 * sizeof(int));
