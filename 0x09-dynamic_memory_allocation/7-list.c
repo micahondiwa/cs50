ba90f9702c909b0a5cfad2c6908ecd151ce83511
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-06 16:10:04 +0300

    Update 7-list.c

--- 0x09-dynamic_memory_allocation/7-list.c
+++ 0x09-dynamic_memory_allocation/7-list.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+/**
+* main: Printing a list of size 3.
+*
+* Return: no return.
+*/
+
+
 int main(void)
 {
     int list[3];
-    list[0]=1;
-    list[1]=2;
-    list[2]=3;
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
 
-    for(int i=0;i<3;i++)
+    for(int i = 0; i < 3; i++)
     {
-        printf("%i\n",list[i]);
+        printf("%i\n", list[i]);
     }
 }
