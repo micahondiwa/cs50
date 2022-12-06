cc9a37c05109f0a30fd2b262ac14589766aa60cc
Micah Ondiwa <micah.oketch.ondiwa@gmail.com>
2022-12-06 15:44:33 +0300

    Update 2-int_compare.c

--- 0x08-pointer_arithmetic/2-int_compare.c
+++ 0x08-pointer_arithmetic/2-int_compare.c
@@ -1,6 +1,12 @@
 #include<cs50.h>
 #include<stdio.h>
 
+/**
+* main: uses pointers to comapre 2 integers and print s"Same" or "Difference".
+*
+* Return: no return.
+*/
+
 int main (void)
 {
     int i = get_int("i: ");
